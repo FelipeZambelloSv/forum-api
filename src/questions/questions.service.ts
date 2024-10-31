9ca88183f0426aadce01c8e4e9a2bebc6049be1b
FelipeZambelloSv <rm99843@fiap.com.br>
2024-10-31 16:45:22 -0300

    feat: Add AnswersModule to AppModule

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -9,8 +9,7 @@ export class QuestionsService {
   @Inject()
   private readonly prisma: PrismaService;
 
-  async create(createQuestionDto: CreateQuestionDto) {
-    const userId = 1;
+  async create(createQuestionDto: CreateQuestionDto, userId: number) {
     return await this.prisma.questions.create({
       data: { ...createQuestionDto, userId }
     });
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,11 @@
+import { Users } from "@prisma/client";
+
+
+export class User implements Users {
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
